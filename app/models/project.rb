8f555d0464755663ef244721e004a10fadaac2a5
benny <benny08118@gmail.com>
2021-01-06 20:02:25 +0800

    fix project associate

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,5 @@ class Project < ApplicationRecord
   #validates :salesman, presence: true #業務   
   validates :PM, presence: true #負責PM    
   has_many :test_modes
-  belongs_to :user
   has_many :comments, as: :commentable
 end
