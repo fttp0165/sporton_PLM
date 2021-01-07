f7d98783930780ce63ceb990b652e1768d430cbb
benny <benny08118@gmail.com>
2021-01-07 23:43:27 +0800

    fix select tag can display current option on edit page

--- app/javascript/script/statuslight.js
+++ app/javascript/script/statuslight.js
@@ -3,6 +3,14 @@ document.addEventListener("turbolinks:load", ()=>{
       let p=document.querySelectorAll(".statusLight");
       for(i=0;i<p.length;i++){
         console.log(p[i].dataset.status);
+        if(p[i].dataset.status == "未完成"){
+            p[i].classList.add("doit");
+        }else if(p[i].dataset.status == "完成"){
+            p[i].classList.add("finish");
+        }else if(p[i].dataset.status == "pending"){
+            p[i].classList.add("pending");
+        }
+        
       }
       
 })
